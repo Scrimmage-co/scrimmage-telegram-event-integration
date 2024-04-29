e65ac8fd610c79ae6503eabb7bd8c6841edcf98d
yevgeniusr <yev.rachkovan@gmail.com>
2024-04-29 13:18:22 +0700

    Add widged UI and validation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Configuration } from './configurations';
+import * as crypto from 'crypto';
+import { ScrimmageService } from './services/scrimmage.service';
+import { validate } from './utils/validation';
 
 @Controller()
-export class AppController {}
+export class AppController {
+  constructor(
+    private configService: ConfigService<Configuration>,
+    private scrimmageService: ScrimmageService,
+  ) {}
+  @Post('api/auth')
+  async authorizeUser(@Body() body: any) {
+    validate(body, this.configService.get('TELEGRAM_TOKEN'));
+    const token = await this.scrimmageService.registerUser({
+      userId: body.user.id,
+      username: body.username,
+    });
+
+    return {
+      token,
+    };
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,17 @@ import { ConfigModule } from '@nestjs/config';
 import { ScrimmageService } from './services/scrimmage.service';
 import { loadDotEnvConfiguration } from './configurations';
 import { TelegramService } from './services/telegram.service';
-
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [loadDotEnvConfiguration],
       isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+      exclude: ['/api/(.*)'],
     }),
   ],
   controllers: [AppController],
